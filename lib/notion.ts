f29cb128eb127a08acf4999a9451a65abc934e18
hmintoh <hmintoh@gmail.com>
2021-07-26 15:47:31 +0800

    fix(lib/notion): API token

--- lib/notion.ts
+++ lib/notion.ts
@@ -4,9 +4,11 @@ const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
 
+const databaseId = process.env.NOTION_DATABASE_ID;
+
 export const getLocations = async () => {
   const res = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
+    database_id: databaseId,
   });
 
   return {
