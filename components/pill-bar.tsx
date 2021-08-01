b6b1e624ba6ecd75f531c4ed2e98c23f9388582c
hmintoh <hmintoh@gmail.com>
2021-08-02 00:09:14 +0800

    feat(components/pill-bar): create component

---
+++ components/pill-bar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { PillBarWrapper, Pill } from "@/styles/pill-bar.styles";
+
+interface PillBarProps {
+  labels: string[];
+  setActive: (string) => void;
+}
+
+const PillBar = ({ labels, setActive }: PillBarProps): JSX.Element => {
+  const [activePill, setActivePill] = useState<number | undefined>(undefined);
+
+  const handleClick = (i, label) => {
+    setActivePill(i);
+    setActive(label);
+  };
+
+  return (
+    <PillBarWrapper role="tablist">
+      {labels.map((label, i) => (
+        <Pill
+          key={i}
+          role="tab"
+          onClick={() => handleClick(i, label)}
+          value={label}
+          active={activePill === i}
+        >
+          {label}
+        </Pill>
+      ))}
+    </PillBarWrapper>
+  );
+};
+
+export default PillBar;
---
+++ styles/pill-bar.styles.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { spacing, theme } from "@/styles/tokens";
+
+export const PillBarWrapper = styled.div`
+  padding: ${spacing._20} 0;
+  display: flex;
+
+  & > :not(:last-child) {
+    margin-right: ${spacing._8};
+  }
+`;
+
+export const Pill = styled.button`
+  padding: ${spacing._2} ${spacing._8};
+  border-radius: ${spacing._8};
+  border: ${spacing._1} solid ${theme.secondary.reg};
+  background: ${(props) => (props.active ? theme.secondary.light : "none")};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
